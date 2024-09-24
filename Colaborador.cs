c3c041502baeebc06fd315550086a71e6dde32bf
Rickzin11 <118747908+Rickzin11@users.noreply.github.com>
2024-09-24 14:56:01 -0300

    Add files via upload

---
+++ Colaborador.cs
@@ -0,0 +1,13 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace Projeto_de_Software.Controllers;
+
+public class ColaboradorController : Controller
+{
+
+    public IActionResult Index()
+    {
+        return View();
+    }
+
+}
---
+++ HomeController.cs
@@ -0,0 +1,26 @@
+using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using Projeto_de_Software.Models;
+
+namespace Projeto_de_Software.Controllers;
+
+public class HomeController : Controller
+{
+
+    public IActionResult Index()
+    {
+        return View();
+    }
+
+    public IActionResult Privacy()
+    {
+        return View();
+    }
+
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Error()
+    {
+        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+    }
+
+}
